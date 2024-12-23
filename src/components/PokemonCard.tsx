4844143f133ac69fc5846396f13925f991eec2c5
Davide <doreglio@red.software.systems>
2024-12-23 10:01:28 +0100

    add type safety to PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,29 @@
-type Props = {
+// PokemonCard.tsx
+import React from "react";
+
+export type PokemonCardProps = {
   name: string;
-  image: string;
+  detailsUrl: string;
 };
 
-export default function PokemonCard({ name, image }: Props) {
+const PokemonCard: React.FC<PokemonCardProps> = ({ name, detailsUrl }) => {
+  const getImageUrl = (url: string): string => {
+    const imageUrlPrefix =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+    const id = url.split("/").slice(-2, -1)[0];
+    return `${imageUrlPrefix}/${id}.png`;
+  };
+
   return (
     <div className="border p-4 rounded shadow hover:shadow-lg">
-      <img src={image} alt={name} className="w-20 h-20 mx-auto" />
+      <img
+        src={getImageUrl(detailsUrl)}
+        alt={name}
+        className="w-20 h-20 mx-auto"
+      />
       <p className="text-center mt-2 font-bold capitalize">{name}</p>
     </div>
   );
-}
+};
+
+export default PokemonCard;
--- src/pages/Fetch.tsx
+++ src/pages/Fetch.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import PokemonCard from "../components/PokemonCard";
+import PokemonCard, { PokemonCardProps } from "../components/PokemonCard";
 import Section from "../components/Section";
 
-async function fetchPokemons() {
+async function fetchPokemons(): Promise<PokemonCardProps[]> {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=3");
   const data = await response.json();
   return data.results; //.filter((pokemon: any) => pokemon.name.startsWith("f"));
@@ -11,7 +11,7 @@ async function fetchPokemons() {
 
 export default function Fetch() {
   // State and effect for fetch() example
-  const [pokemons, setFetchPokemons] = useState([] as any[]);
+  const [pokemons, setFetchPokemons] = useState([] as PokemonCardProps[]);
   useEffect(() => {
     fetchPokemons().then(setFetchPokemons);
   }, []);
@@ -30,14 +30,8 @@ export default function Fetch() {
     <>
       <Section heading="Pokemons fetched with fetch()">
         <div className="grid grid-cols-3 gap-4">
-          {pokemons.map((pokemon) => (
-            <PokemonCard
-              key={pokemon.name}
-              name={pokemon.name}
-              image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url
-                .split("/")
-                .slice(-2, -1)}.png`}
-            />
+          {pokemons.map((p) => (
+            <PokemonCard key={p.name} name={p.name} detailsUrl={p.detailsUrl} />
           ))}
         </div>
       </Section>
@@ -45,15 +39,13 @@ export default function Fetch() {
       <Section heading="Pokemons fetched with React Query">
         {isLoading && <p>Loading...</p>}
         {isError && <p>Error loading Pokémons.</p>}
-        {!isLoading && !isError && (
+        {queryPokemons && !isLoading && !isError && (
           <div className="grid grid-cols-3 gap-4">
-            {queryPokemons.map((pokemon: any) => (
+            {queryPokemons.map((p: PokemonCardProps) => (
               <PokemonCard
-                key={pokemon.name}
-                name={pokemon.name}
-                image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url
-                  .split("/")
-                  .slice(-2, -1)}.png`}
+                key={p.name}
+                name={p.name}
+                detailsUrl={p.detailsUrl}
               />
             ))}
           </div>
